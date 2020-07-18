741de9d7d770d0f93c8afceab580151e03f30729
Jefferson <jefferson08jsa@hotmail.com7>
2020-07-18 15:45:08 -0300

    criando o projeto carteira-legal

---
+++ src/main/java/br/com/jsa/carteiralegal/CarteiraLegalApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsa.carteiralegal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraLegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraLegalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jsa/carteiralegal/CarteiraLegalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jsa.carteiralegal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraLegalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
